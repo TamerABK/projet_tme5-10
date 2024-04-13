ad5b6bc8ec56896aadd0e5b9efd57731ec5c4d77
ckaEmma <156218237+ckaEmma@users.noreply.github.com>
2024-04-13 16:05:27 +0200

    Update ReconstitueReseau.c

--- ReconstitueReseau.c
+++ ReconstitueReseau.c
@@ -27,9 +27,16 @@ int main(int argc,char** argv){
 
     Chaines* chaine=lectureChaines(fLecture);
 
+    clock_t debut, fin; 
+
     if(methode_choisie==1)
     {
+        debut = clock(); // début mesure du temps
         Reseau* reseau_listeCh=reconstitueReseauListe(chaine);
+        fin = clock(); // fin mesure du temps
+        double temps_execution = ((double) (fin - debut)) / CLOCKS_PER_SEC; // Calcul du temps écoulé
+        fprintf(fEcriture, "Temps d'exécution pour la méthode Liste : %f secondes\n", temps_execution); // Enregistrement du temps dans le fichier de sortie
+        
         afficheReseauSVG(reseau_listeCh,"testreseauLCH");
         ecrireReseau(reseau_listeCh,fEcriture);
         liberer_reseau(reseau_listeCh);
@@ -37,7 +44,12 @@ int main(int argc,char** argv){
     
     if(methode_choisie==2)
     {
+        debut = clock();
         Reseau* reseau_tableH=reconstitueReseauHachage(chaine,50);
+        fin = clock();
+        double temps_execution = ((double) (fin - debut)) / CLOCKS_PER_SEC;
+        fprintf(fEcriture, "Temps d'exécution pour la méthode Hachage : %f secondes\n", temps_execution);
+        
         afficheReseauSVG(reseau_tableH,"testreseauH");
         ecrireReseau(reseau_tableH,fEcriture);
         liberer_reseau(reseau_tableH);
@@ -45,10 +57,19 @@ int main(int argc,char** argv){
 
     if(methode_choisie==3)
     {
+        debut = clock();
         Reseau* reseau_arbreQ=reconstitueReseauArbre(chaine);
+        fin = clock();
+        double temps_execution = ((double) (fin - debut)) / CLOCKS_PER_SEC;
+        fprintf(fEcriture, "Temps d'exécution pour la méthode ArbreQuat : %f secondes\n", temps_execution);
+        
         afficheReseauSVG(reseau_arbreQ,"testreseauAQ");
         ecrireReseau(reseau_arbreQ,fEcriture);
         liberer_reseau(reseau_arbreQ);
+    }
+
+    if(methode_choisie > 3){
+        printf("Méthode inexistante, veuillez choisir 1 2 ou 3\n");
     }
 
     liberer_chaine(chaine);
@@ -57,3 +78,4 @@ int main(int argc,char** argv){
 
     return 0;
 }
+
